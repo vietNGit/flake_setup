4ae5559506a2e2d220d10491d63c9492b36809c6
Viet <gavinvietnguyen2710@gmail.com>
2024-08-26 14:04:10 +0700

    Add garbage collection

--- flakes/flake.nix
+++ flakes/flake.nix
@@ -11,12 +11,21 @@
 	outputs = { self, nixpkgs, ... }@inputs :
 		let
 			lib = nixpkgs.lib;
+			config = nixpkgs.config;
 		in {
 			nixosConfigurations = {
 				nixosLaptop = lib.nixosSystem {
 					system = "x86_64-linux";
 					specialArgs = inputs;
 					modules = [ ./hosts/laptop/configuration.nix ];
+
+					config = {
+						nix.gc = {
+							automatic = true;
+							dates = "weekly";
+							options = "--delete-older-than +10";
+						};
+					};
 				};
 			};
 		};
