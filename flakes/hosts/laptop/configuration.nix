a20e2ab44f4f2198cdb1a28389cb63ba281d7e85
Viet <gavinvietnguyen2710@gmail.com>
2024-08-26 14:08:17 +0700

    Fix garbage collection for laptop

--- flakes/hosts/laptop/configuration.nix
+++ flakes/hosts/laptop/configuration.nix
@@ -172,4 +172,10 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
 }
