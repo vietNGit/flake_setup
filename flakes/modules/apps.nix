719bf86078198a9332069adb6ab431e6973795fa
Viet <gavinvietnguyen2710@gmail.com>
2024-09-27 22:44:28 +0700

    Idek

--- flakes/modules/apps.nix
+++ flakes/modules/apps.nix
@@ -15,7 +15,7 @@
         package = pkgs-unstable.mullvad-vpn;
       };
     };
-    environment.systemPackages = with pkgs; [
+    environment.systemPackages = (with pkgs; [
       vim
       kitty
       tree
@@ -49,6 +49,19 @@
       quickemu
       virt-manager
       virtualbox
-    ];
+    ])
+    ++ (with pkgs-unstable; [
+      firefox
+      google-chrome
+
+      fastfetch
+      vscode
+      jetbrains.idea-community
+      postman
+
+      appflowy
+      affine
+      libreoffice-fresh
+    ]);
   };
 }
