958808d5417ea4ca3f67278a72e80620c09a336d
vietNGit <gavinvietnguyen2710@gmail.com>
2025-01-03 22:50:07 +0700

    Stuff

--- flakes/modules/gaming.nix
+++ flakes/modules/gaming.nix
@@ -1,9 +1,10 @@
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-stable, ... }:
 
 {
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
+    package = pkgs-stable;
   };
   programs.gamemode.enable = true;
 
