0ae25f27c7278dc0e46270492f64238b9d93267e
vietNGit <gavinvietnguyen2710@gmail.com>
2025-04-04 15:28:20 +0700

    Modify power profile and set battery savings

--- flakes/hosts/laptop/configuration.nix
+++ flakes/hosts/laptop/configuration.nix
@@ -57,7 +57,22 @@
 
   # Setting system power controls
   services.power-profiles-daemon.enable = false;
-  services.tlp.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_performance";
+
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
+      START_CHARGE_THRESH_BAT0 = 70;
+      STOP_CHARGE_THRESH_BAT0 = 90;
+    };
+  };
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
