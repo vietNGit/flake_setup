7530fcf1e4583b62692c0cbfd9fc76d9c6386deb
vietNGit <gavinvietnguyen2710@gmail.com>
2025-04-16 21:13:17 +0700

    Flakes updated or modified Wed 16 Apr 2025 21:07:56 +07

--- flakes/modules/fonts.nix
+++ flakes/modules/fonts.nix
@@ -1,10 +1,23 @@
 { config, pkgs, ... }:
 
 {
+  # fonts = {
+  #   enableDefaultPackages = true;
+  #   packages = with pkgs; [
+  #     nerd-fonts.meslo-lg
+  #   ];
+  # };
   fonts = {
     enableDefaultPackages = true;
     packages = with pkgs; [
-      nerd-fonts.meslo-lg
+      (nerdfonts.override { fonts = [ "Meslo" ]; })
     ];
+
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = [ "Meslo LG M Regular Nerd Font Complete Mono" ];
+      };
+    };
   };
 }
