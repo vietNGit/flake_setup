3f1592e17eedc38196e6d570b179097d2761385b
Gaverino <gavinvietnguyen2710@gmail.com>
2026-07-02 10:44:18 +0700

    Add nix darwin config, comment out nixos flake config for now, will reconcile the 2 setup in later updates (#2)
    * Initial commit moving to new setup with account for nix darwin
    
    * Update vars to reflect new dir

---
+++ flakes/home.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "vietmacbook";
+  home.homeDirectory = "/Users/vietmacbook";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "26.05";
+
+  programs = {
+    ghostty = {
+      enable = true;
+
+      # 1. Tell Home Manager NOT to install the ghostty package from nixpkgs
+      package = null;
+
+      settings = {
+        theme = "Catppuccin Frappe";
+        font-size = 16;
+        font-family = "MesloLGM Nerd Font Mono";
+      };
+    };
+
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+
+    zsh = {
+      enable = true;
+      oh-my-zsh = {
+        enable = true;
+        theme = "ys";
+        plugins = [
+          "git"
+          "direnv"
+        ];
+      };
+    };
+  };
+}
---
+++ shell_setup/aliases.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Bash aliases =================================================================
+alias show_bash="cat ~/.bashrc"
+alias re_source="source ~/.bashrc"
+
+# ==============================================================================
+
+# Nixos channels aliases =======================================================
+
+alias mod_nix="sudo nano /etc/nixos/configuration.nix"
+alias nix_reload="sudo nixos-rebuild switch"
+alias nix_upgrade="sudo nixos-rebuild switch --upgrade"
+alias nix_full_update="echo 'Update channel and upgrade packages' && sudo nix-channel --update && sudo nixos-rebuild switch --upgrade"
+
+# ==============================================================================
+
+# Nixos flakes aliases =========================================================
+
+alias flake_update="nix flake update"
+alias flake_rebuild="sudo nixos-rebuild switch --flake"
+
+# ==============================================================================
+
+# General aliases ==============================================================
+
+alias ff="fastfetch"
+alias ffa="fastfetch -c all"
+alias dtb="distrobox"
+command -v bat >/dev/null 2>&1 && alias cat=bat
+alias reload_current_shell_session="exec $SHELL"
+command -v nvim >/dev/null 2>&1 && alias vim=nvim
+command -v nvim >/dev/null 2>&1 && alias vi=nvim
+
+# ==============================================================================
+
+# Nix Darwin specific aliases ==================================================
+
+alias home_manager_switch="nix run home-manager switch --flake $FLAKE_PROJECT_ROOT/flakes#macbookPro14"
+alias darwin_flake_update="nix flake update --flake $FLAKE_PROJECT_ROOT/flakes" # TODO: Rebuild this to use the flake path from the config file instead of hardcoding it
+alias rebuild_darwin="sudo darwin-rebuild switch --flake $FLAKE_PROJECT_ROOT/flakes#macbookPro14"
+
+# ==============================================================================
