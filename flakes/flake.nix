50efd7be5482a32180cc26fd2931ba772dfe6de7
vietNGit <gavinvietnguyen2710@gmail.com>
2026-08-05 17:32:42 +0700

    Flakes updated or modified Wed  5 Aug 2026 17:25:08 +07

--- flakes/flake.nix
+++ flakes/flake.nix
@@ -4,8 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/dea0d9eeca494734e596f3f4a813324d6af41265";
     # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
